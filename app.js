78bf3ba0f94202277c230911cea3b595ebc30844
mojtaba1996cs <mojtaba1996cs@gmail.com>
2025-11-03 03:53:14 +0200

    gg

--- app.js
+++ app.js
@@ -5,8 +5,11 @@ const mongoose = require('mongoose')
 app.get('/', (req, res) => {
  res.sendFile("./views/home.html", {root: __dirname});
 })
-
-app.listen(port, () => {
-  console.log(`http://localhost:${port}/`)
-})
-
+mongoose.connect("mongodb+srv://taboodb:mojaba1996cs2@cluster0.ursadu6.mongodb.net/?appName=Cluster0").then(() => {
+  app.listen(port, () => {
+    console.log(`http://localhost:${port}`);
+  });
+ })
+ .catch((err) => {
+   console.log(err);
+ });
